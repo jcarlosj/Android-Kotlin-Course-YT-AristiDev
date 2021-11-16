aae426a71b8ccd37860a16d662c782125a46f791
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-16 18:49:33 -0500

    [+] MainActivity: Conecta Adapter con ReciclerView

--- app/src/main/java/com/github/jcarlosj/MainActivity.kt
+++ app/src/main/java/com/github/jcarlosj/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.jcarlosj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +21,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.rvSuperHero)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        val adapter = HeroAdapter( superHeroes )
+        recyclerview.adapter = adapter
+
     }
+
 }
