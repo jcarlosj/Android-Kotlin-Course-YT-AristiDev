f3c94060afb3c33418affe661e58f80c69dbd67d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-16 12:08:01 -0500

    [+] MainActivity: Valida EditView

--- app/src/main/java/com/github/jcarlosj/MainActivity.kt
+++ app/src/main/java/com/github/jcarlosj/MainActivity.kt
@@ -2,10 +2,33 @@ package com.github.jcarlosj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShowName = findViewById<Button>( R.id.btnShowName )
+        btnShowName.setOnClickListener {
+            checkValue()
+        }
+    }
+
+    fun checkValue() {
+        var etName = findViewById<EditText>( R.id.etName )
+        if( etName.text.isNotEmpty() ) {
+            Log.d( "MainActivity","Envia datos a un nuevo Activity" )
+        }
+        else {
+            showErrorName()
+        }
+    }
+
+    fun showErrorName() {
+        Toast.makeText( this, "El campo no puede estar vacio", Toast.LENGTH_LONG ) .show()
     }
 }
