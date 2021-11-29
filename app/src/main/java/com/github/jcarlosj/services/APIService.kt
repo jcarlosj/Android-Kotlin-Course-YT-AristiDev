5a097713533959f2381e8a3fe6969284a8e027d3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-29 16:16:12 -0500

    [+] Interface: APIService

---
+++ app/src/main/java/com/github/jcarlosj/services/APIService.kt
@@ -0,0 +1,11 @@
+package com.github.jcarlosj.services
+
+import com.github.jcarlosj.models.Dog
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+    @GET
+    fun getDogsByBreed( @Url url:String ) : Response<Dog>
+}
