b60bc689adf9ea46ef545f24d35e5bdeb78df829
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-29 15:58:29 -0500

    [+] Data Class: SerializedName Dog Class

--- app/src/main/java/com/github/jcarlosj/models/Dog.kt
+++ app/src/main/java/com/github/jcarlosj/models/Dog.kt
@@ -1,6 +1,9 @@
 package com.github.jcarlosj.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Dog(
-    private val status: String,
-    private val message: List<String>
+    /** SerializedName contendrá los nombres de las propiedades de la API (status, message), sus equivalentes dentro de la aplicación serán los que se encuentran fuera (status, urlImages ) */
+    private @SerializedName( "status" ) val status: String,
+    private @SerializedName( "message" ) val urlImages: List<String>
 )
