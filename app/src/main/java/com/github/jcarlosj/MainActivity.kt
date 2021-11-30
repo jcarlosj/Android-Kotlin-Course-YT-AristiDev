6833bdc757d247b2b4c908ea972755b18052e619
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-29 19:35:17 -0500

    [+] MainActivity: Despliega data RecyclerView al buscar usando SearchView (Obtenida de API)
    Para desplegar la data obtenida del API:
    0. Se establece una lista mutable vacia para mantener la data que se mostrara como resultado de la busqueda;
    1. Se establece un Listener para el elemento SearchView implementando SearchView.OnQueryTextListener
    2. Se controla la escucha con el metodo onQueryTextSubmit
    3. Se realiza la busqueda a traves de una Coroutine que controla la peticion al API a traves de Retrofit2, una
       Interfaz con implementacion de un metodo asincrono (suspend) en un hilo secundario
    4. Al obtener el resultado de la data del API con Retrofit se obtiene del cuerpo de la respuesta
    5. La data del cuerpo de la respuesta es asignada la lista mutable a la que previamente se eliminan todos sus items
    6. Se indica al Adapter que la data a cambiado para que la nueva lista mutable con los nuevos datos pueda actualizarse
       en el RecyclerView, que previamente se a definido junto con el respectivo Adapter.

--- app/src/main/java/com/github/jcarlosj/MainActivity.kt
+++ app/src/main/java/com/github/jcarlosj/MainActivity.kt
@@ -2,15 +2,89 @@ package com.github.jcarlosj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.jcarlosj.adapters.DogAdapter
 import com.github.jcarlosj.databinding.ActivityMainBinding
+import com.github.jcarlosj.models.Dog
+import com.github.jcarlosj.services.APIService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: DogAdapter
+    private val listImages = mutableListOf<String>()                        //  Define una lista mutable vacia
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate( layoutInflater )
         setContentView( binding.root )
+
+        binding.svData.setOnQueryTextListener( this )                       //  Agrega un Listener al SearchView
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapter = DogAdapter( listImages )                                  // Asigna la lista mutable vacia al adapter
+        binding.rvData.layoutManager = LinearLayoutManager( this )  //  Crea el LayoutManager para el RecyclerView
+        binding.rvData.adapter = adapter                                    // Asigna el Adapter al RecyclerView
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl( "https://dog.ceo/api/breed/")          // Agrega URL del API. Siempre dejar un backslash (/) al final
+                .addConverterFactory( GsonConverterFactory.create() )   // Convierte el JSON obtenido a un Objeto
+                .build()
+
+    }
+
+    // Hace la busqueda por raza
+    private fun searchByBreed( term: String ) {
+        // Llamada asincrona en hilo secundario usando una coroutine
+        CoroutineScope( Dispatchers.IO ) .launch {
+            val response: Response<Dog> = getRetrofit() .create( APIService::class.java ) .getDogsByBreed( "$term/images" )
+            var data: Dog? = response.body()
+
+            // LLamada de ejecucion sobre el hilo principal
+            runOnUiThread {
+                if( response.isSuccessful ) {
+                    val images: List<String> = data?.urlImages ?: emptyList()      //  Obtiene la data del Response, si se establecer como null entonces pasa una lista vacia
+
+                    Log.d( "images", images.size.toString() )
+
+                    listImages.clear()                                              //  Elimina todos los items de la lista mutable
+                    listImages.addAll( images )                                     //  Agrega la data obtenida del Response
+                    adapter.notifyDataSetChanged()                                  //  Indica al adapter que hubo cambios en la lista mutable que se le paso al principio
+                }
+                else { showError() }
+            }
+        }
+    }
+
+    private fun showError() {
+        Toast.makeText( this, "Ha ocurrido un error", Toast.LENGTH_SHORT ).show()
+    }
+
+    /** Members: SearchView.OnQueryTextListener */
+    // Submit: Realiza la busqueda del termino en el SearchView
+    override fun onQueryTextSubmit( query: String? ): Boolean {
+        // Si lo que hay en el SearchView no es vacio ni nulo
+        if( !query .isNullOrEmpty() ) {
+            searchByBreed( query.lowercase() )
+        }
+
+        return true
+    }
+    // Obtiene los cambios al agregar algo al SearchView
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 }
