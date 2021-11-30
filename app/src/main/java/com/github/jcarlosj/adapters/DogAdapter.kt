5e6fb5bfacdd7236ffcc2237046dc372e0b85b87
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-11-29 19:29:39 -0500

    [+] Adapter: DogAdapter, DogViewHolder

--- app/src/main/java/com/github/jcarlosj/adapters/DogAdapter.kt
+++ app/src/main/java/com/github/jcarlosj/adapters/DogAdapter.kt
@@ -1,4 +1,29 @@
 package com.github.jcarlosj.adapters
 
-class DogAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.jcarlosj.R
+
+class DogAdapter( private val items:List<String> ) : RecyclerView.Adapter<DogViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        return DogViewHolder(
+            LayoutInflater.from( parent.context )
+            .inflate(
+                R.layout.layout_item_dog,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        val item = items[ position ]
+
+        holder.bind( item )
+    }
+
+    override fun getItemCount(): Int = items.size
+
 }
--- app/src/main/java/com/github/jcarlosj/adapters/DogViewHolder.kt
+++ app/src/main/java/com/github/jcarlosj/adapters/DogViewHolder.kt
@@ -1,4 +1,15 @@
 package com.github.jcarlosj.adapters
 
-class DogViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.github.jcarlosj.databinding.LayoutItemDogBinding
+import com.squareup.picasso.Picasso
+
+class DogViewHolder( view: View ) : RecyclerView.ViewHolder( view ) {
+
+    private val binding = LayoutItemDogBinding.bind( view )
+
+    fun bind( urlImage: String ) {
+        Picasso.get().load( urlImage ).into( binding.ivDog )
+    }
 }
